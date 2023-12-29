3308177da2ac604d2861276383219c36a56887d2
Hamzshaa <trident32000@gmail.com>
2023-12-29 04:39:20 -0800

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 //mongodb://127.0.0.1:27017
 mongoose.connect(
-  "mongodb+srv://trident:yMf25toKWG4Dqevi@postdb.ryg6kpw.mongodb.net/todolistDB"
+  "mongodb+srv://trident:yMf25toKWG4Dqevi@postdb.ryg6kpw.mongodb.net/toDB"
 );
 
 const itemsSchema = {
